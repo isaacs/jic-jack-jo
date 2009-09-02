4f5862fb64c908f7fe3062d9e1412461b55629a8
isaacs <i@foohack.com>
2009-09-02 01:16:50 -0700

    Making more headway.  Got diverted to build an http client for interacting with CouchDB.  Divided work up into /game and /move WS groups.

--- jackconfig.js
+++ jackconfig.js
@@ -7,26 +7,67 @@ var Jack = require("jack"), map = {};
 //     }
 // };
 
+// var toJSON = function (app) {
+//     return function (env) {
+//         var result = app(env);
+//         var type = require("hashp").HashP.get(result[1], "Content-Type") || '';
+//         if (type.match(/\bjson\b/)) {
+//             result[2] = [JSON.stringify(result[2])];
+//         }
+//         return result;
+//     };
+// };
+
 map["/test"] = function (env) {
-    var keys = [];
+    var req = new Jack.Request(env);
+    
+    var body = [
+        "<!DOCTYPE html><head><title>test</title></head>",
+        "<body>"
+    ];
+    
+    
+    body.push("<form action='' method=post><input name=foo value=bar type=submit></form>");
+    
+    body.push(req.POST("foo") || 'no foo!');
+    body.push("<br>");
+    
+    body.push(JSON.stringify(req.POST()) || "huh?");
+    
+    body.push("<br>gets: <br>")
+    body.push(JSON.stringify(req.GET()) || "huh?");
+    // body.push("<hr>");
+    // body.push(JSON.stringify(env["jack.request"]));
+    
+    
+    body.push("<pre>");
     for (var i in env) {
-        keys.push(i+":"+typeof(env[i])+" "+env[i]+"\n");
+        body.push(i+":"+typeof(env[i])+" "+env[i]+"\n");
     }
-    return [200, {"Content-type":"text/plain"}, keys];
+    body.push("</pre>");
+    
+    
+    return new Jack.Response(200, {}, body).finish();
+
 };
 
 // map the requests to handlers
 require("hashp").HashP.update(map, require("./lib/urlmap"));
 exports.app = Jack.URLMap(map);
 
-// serve static stuff from the /static folder.
-exports.app = Jack.Static(exports.app, { urls : ["/static"] });
-
 // attach the content-length header.
 exports.app = Jack.ContentLength(exports.app);
 
-// support bodiless HEAD request.
+
+// serve static stuff from the /static folder.
+exports.app = Jack.Static(exports.app, { urls : ["/static"] });
+
+// support bodiless HEAD requests
 exports.app = Jack.Head(exports.app);
 
 // support method swapping
 exports.app = Jack.MethodOverride(exports.app);
+
+// jsonp on anything!
+exports.app = Jack.JSONP(exports.app);
+
