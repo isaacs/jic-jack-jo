3ca28313d15f1c1695501418543ff5e4e8f526b0
Matt Hackett <matth@paletteswap.com>
2009-09-04 00:32:32 -0700

    adding board.js, nothing useful there yet

---
+++ tic-tac-toe/board.js
@@ -0,0 +1,38 @@
+/*
+exports = exports || {};
+
+exports.Board = function() {
+
+    this.init = function() {
+
+
+
+    };
+
+    this.move = function() {
+    };
+
+    this.newGame = function() {
+      [xoo]
+      [---]
+      [---]
+    };
+
+    this.tiles = [];
+
+};
+
+//////////////////
+
+var Board = require("game/board").Board;
+
+b = new Board();
+
+b.newGame();
+
+b.on('move', function(e) {
+
+  // hit douchdb
+
+});
+*/
